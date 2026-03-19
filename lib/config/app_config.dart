838fd0081ac196fcefbee5e4ae677240740043f9
elchinito24 <scarface_24@hotmail.es>
2026-03-19 04:03:32 -0700

    Update app_config.dart
    Tomara la base_url automaticamente cuando creemos la app mediante el comando

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,6 +1,8 @@
 class AppConfig {
-  //static const String baseUrl = "http://172.10.108.58:8000/api/v1";
-  static const String baseUrl = "http://10.0.2.2:8000/api/v1";
+  static const String baseUrl = String.fromEnvironment(
+    'BASE_URL',
+    defaultValue: 'http://172.10.108.58:8000/api/v1',
+  );
   static const int connectTimeout = 10;
   static const int receiveTimeout = 10;
 }
